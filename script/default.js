e6d6188a7c8704dc1468396736a7474f8c516c0c
WilliamB <30449912+billbic@users.noreply.github.com>
2026-04-24 11:20:31 -0400

    Formatted

--- script/default.js
+++ script/default.js
@@ -1,37 +1,34 @@
 "option strict";
 
 let meta = {
-    blockUI: {
-        load: "<h4>Loading...</h4>"
-    }
+  blockUI: {
+    load: "<h4>Loading...</h4>",
+  },
 };
 
 $(document).ready(function () {
-    try {
-        console.log("DOM ready - default.js loaded");
+  try {
+    console.log("DOM ready - default.js loaded");
 
-        ErrorHandler.init( window.location.hostname === "localhost");
-
-    } catch (error) {
-      ErrorHandler.handle(e,"$(document).ready");  
-    }
-    
+    ErrorHandler.init(window.location.hostname === "localhost");
+  } catch (error) {
+    ErrorHandler.handle(e, "$(document).ready");
+  }
 });
 
 function blockUI(msg) {
-    try {
-        $.blockUI({
-            message: msg || "<h4>Loading...</h4>",
-            css: {
-                border: 'none',
-                padding: '15px',
-                backgroundColor: '#000',
-                color: '#fff',
-                borderRadius: '8px'
-            }
-        });
-
-    } catch (e) {
-        ErrorHandler.handle(e, "blockUI");
-    }
+  try {
+    $.blockUI({
+      message: msg || "<h4>Loading...</h4>",
+      css: {
+        border: "none",
+        padding: "15px",
+        backgroundColor: "#000",
+        color: "#fff",
+        borderRadius: "8px",
+      },
+    });
+  } catch (e) {
+    ErrorHandler.handle(e, "blockUI");
+  }
 }
